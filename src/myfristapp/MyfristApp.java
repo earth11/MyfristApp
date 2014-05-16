8712b6a0ddfff9d2fca8ee87ad2c2fd0223191ce
earth11 <gm_earthserv@hotmail.com>
2014-05-16 15:08:25 +0700

    แนะนำตัว

--- src/myfristapp/MyfristApp.java
+++ src/myfristapp/MyfristApp.java
@@ -16,7 +16,10 @@ public class MyfristApp {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("My name is: Phanuthep Saisuriya");
+        System.out.println("สาขา: Mobile Computing Technology");
+        System.out.println("คณะ: Information and Communication Technology");
+        System.out.println("เรียนที่: University of Phayao");
     }
     
     
